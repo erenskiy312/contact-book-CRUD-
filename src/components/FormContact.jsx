2b8422b40208dffa73ec61a6ba0c1a4936974f9f
erbolken <msuharik2@gmail.com>
2024-03-26 20:40:41 +0600

    fix bag

--- src/components/FormContact.jsx
+++ src/components/FormContact.jsx
@@ -37,7 +37,7 @@ const FormContact = (props) => {
           </li>
         ))}
       </ul> */}
-      <a href="#" onClick={() => navigate('/')}>ADDd</a>
+      <a href="#" onClick={() => navigate('/')}>ADD</a>
         </div>
   )
 }
